1b6e4c437751a6b853868ea724d0ea0d0fd448df
Frans-Willem Post <fwiep@fwiep.nl>
2018-10-14 11:16:11 +0200

    work in progress for PEAR standard

--- index.php
+++ index.php
@@ -1,4 +1,122 @@
 <?php
+/**
+ * Random StarTrek episode selector
+ *
+ * PHP version 7.2
+ *
+ * @category Trekisode
+ * @package  Trekisode
+ * @author   Frans-Willem Post (FWieP) <fwiep@fwiep.nl>
+ * @license  https://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://fwiep.nl/
+ */
+
+/**
+ * StarTrek series
+ *
+ * @category Series
+ * @package  Trekisode
+ * @author   Frans-Willem Post (FWieP) <fwiep@fwiep.nl>
+ * @license  https://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://fwiep.nl/
+ */
+class Series
+{
+    /**
+     * The series' mnemonic, three characters code 
+     * 
+     * @var string
+     */
+    public $code = '';
+    
+    /**
+     * The series' full name
+     * 
+     * @var string
+     */
+    public $name = '';
+    
+    /**
+     * The series' seasons
+     * 
+     * @var Season[]
+     */
+    public $seasons = array();
+    
+    /**
+     * Creates a new series
+     * 
+     * @param string $code the code
+     * @param string $name the name
+     */
+    public function __construct(string $code, string $name) 
+    {
+        $this->code = $code;
+        $this->name = $name;
+    }
+}
+
+/**
+ * StarTrek series' season
+ *
+ * @category Season
+ * @package  Trekisode
+ * @author   Frans-Willem Post (FWieP) <fwiep@fwiep.nl>
+ * @license  https://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://fwiep.nl/
+ */
+class Season
+{
+    /**
+     * @var int
+     */
+    public $number = 0;
+    
+    /**
+     * @var array
+     */
+    public $episodes = array();
+    
+    public function __construct($number) 
+    {
+        $this->number = $number;
+    }
+}
+
+/**
+ * StarTrek episode
+ *
+ * @category Episode
+ * @package  Trekisode
+ * @author   Frans-Willem Post (FWieP) <fwiep@fwiep.nl>
+ * @license  https://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://fwiep.nl/
+ */
+class Episode
+{
+    /**
+     * @var int
+     */
+    public $number = 0;
+    
+    /**
+     * @var string
+     */
+    public $name = '';
+    
+    /**
+     * @var int
+     */
+    public $airdate = 0;
+ 
+    public function __construct($number, $name, $airdate) 
+    {
+        $this->number = $number;
+        $this->name = $name;
+        $this->airdate = $airdate;
+    }
+}
+
 $x = new DOMDocument();
 $o = array();
 $p = dirname(__FILE__);
@@ -11,7 +129,7 @@ if (@$x->load($p.'/data.xml') && @$x->schemaValidate($p.'/schema.xsd')) {
         );
         foreach ($series->getElementsByTagName('Season') as $season) {
             $se = new Season((int)$season->getAttribute('number'));
-
+            
             foreach ($season->getElementsByTagName('Episode') as $episode) {
                 $ep = new Episode(
                     (int)$episode->getAttribute('number'),
@@ -31,11 +149,12 @@ if (@$x->load($p.'/data.xml') && @$x->schemaValidate($p.'/schema.xsd')) {
     
     $outputFormat = "<pre>Today's random StarTrek episode:<br />".
         "<a href=\"http://en.memory-alpha.org/wiki/%1\$s\">%1\$s</a> ".
-        "<a href=\"http://en.memory-alpha.org/wiki/%1\$s_Season_%2\$d\">%2\$dx%3\$02d</a> ".
+        "<a href=\"http://en.memory-alpha.org/wiki/%1\$s_Season_%2\$d\"".
+        ">%2\$dx%3\$02d</a> ".
         "<a href=\"http://en.memory-alpha.org/wiki/%5\$s_(episode)\">%4\$s</a>".
-    "</pre>";
+        "</pre>";
     
-    if (isset ( $argv ) && $argc > 0) {
+    if (isset($argv) && $argc > 0) {
         $outputFormat = "Today's random StarTrek episode:\n".
             "%1\$s %2\$dx%3\$02d %4\$s\n";
     }
@@ -46,68 +165,6 @@ if (@$x->load($p.'/data.xml') && @$x->schemaValidate($p.'/schema.xsd')) {
         $randSeason->number,
         $randEpisode->number,
         $randEpisode->name,
-        str_replace (' ', '_', $randEpisode->name)
+        str_replace(' ', '_', $randEpisode->name)
     );
-}
-
-class Series
-{
-    /**
-     * @var string
-     */
-    public $code = '';
-    
-    /**
-     * @var string
-     */
-    public $name = '';
-    
-    /**
-     * @var array
-     */
-    public $seasons = array();
-    
-    public function __construct($code, $name) {
-        $this->code = $code;
-        $this->name = $name;
-    }
-}
-class Season
-{
-    /**
-     * @var int
-     */
-    public $number = 0;
-    
-    /**
-     * @var array
-     */
-    public $episodes = array();
-    
-    public function __construct($number) {
-        $this->number = $number;
-    }
-}
-class Episode
-{
-    /**
-     * @var int
-     */
-    public $number = 0;
-    
-    /**
-     * @var string
-     */
-    public $name = '';
-    
-    /**
-     * @var int
-     */
-    public $airdate = 0;
- 
-    public function __construct($number, $name, $airdate) {
-        $this->number = $number;
-        $this->name = $name;
-        $this->airdate = $airdate;
-    }
 }
